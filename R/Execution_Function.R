8f2eef88398ed6fe6c7ad42ce28b49200ead5fea
jamesggow08 <james.g.gow.21@gmail.com>
2023-05-05 02:56:27 -0400

    allupload

---
+++ R/Execution_Function.R
@@ -0,0 +1,122 @@
+#' Takes data extracted from EndoPAT graph to determine inputs for iCe for the pre exercise data
+#'
+#'
+#' @param x dataset created by the EndoPAT graph
+#' @param y string used to name the outputs
+#'
+#' @return .csv file of inflection points (minima in first derivative)
+#' @return .text file with description of linear regression of first portion of pulse pressure graph
+#'
+#' @return PulseWave.png of Pulse pressure graph
+#' @return FirstDev.png of Pulse pressure graph first derivative
+#' @return InitialSlope.png of Initial linear portion of the pulse pressure graph
+#' @export
+#'
+#' @import data.table
+#'
+#' @examples iCe_pre("data.csv", "test")
+iCe_pre <- function(x, y) {
+  name <- paste0(y, "_Pre")
+  read_data(x)
+  print ("Data Read Success")
+  calcLocalMax(data2)
+  print ("Max Values Calculated")
+  calcLocalMin(data2)
+  print ("Min Values Calculated")
+  localvalremoveMax(MaximaRows)
+  print ("Max Values Cleaned")
+  localvalremoveMin(MinimaRows)
+  print ("Min Values Cleaned")
+  nonparam(FinalMaxima4, FinalMinima4, name)
+  firstdev(predpulsewave, name)
+  print ("Graphing Done")
+  initialslope(predpulsewave, name)
+  print ("Slope Calculated")
+  inflecpoints(dapulsewave)
+  name <- paste0("inflectionpoints", y, "_Pre.csv")
+  write.table(inflection_points, file=name, sep=",")
+  print ("Inflection Points Found")
+  name <- paste0(y, "_Pre")
+  iCegraph(x = predpulsewave, n=name)
+  print ("New Graph Added")
+}
+
+#' Takes data extracted from EndoPAT graph to determine inputs for iCe for the pre exercise data
+#'
+#'
+#' @param x dataset created by the EndoPAT graph
+#' @param y string used to name the outputs
+#'
+#' @return .csv file of inflection points (minima in first derivative)
+#' @return .text file with description of linear regression of first portion of pulse pressure graph
+#'
+#' @return PulseWave.png of Pulse pressure graph
+#' @return FirstDev.png of Pulse pressure graph first derivative
+#' @return InitialSlope.png of Initial linear portion of the pulse pressure graph
+#' @export
+#'
+#' @examples iCe_demo("data.csv", "test")
+iCe_post <- function(x, y) {
+  name <- paste0(y, "_Post")
+  read_data(x)
+  print ("Data Read Success")
+  calcLocalMax(data2)
+  print ("Max Values Calculated")
+  calcLocalMin(data2)
+  print ("Min Values Calculated")
+  localvalremoveMax(MaximaRows)
+  print ("Max Values Cleaned")
+  localvalremoveMin(MinimaRows)
+  print ("Min Values Cleaned")
+  nonparam(FinalMaxima4, FinalMinima4, name)
+  firstdev(predpulsewave, name)
+  print ("Graphing Done")
+  initialslope(predpulsewave, name)
+  print ("Slope Calculated")
+  inflecpoints(dapulsewave)
+  name <- paste0("inflectionpoints", y, "_Post.csv")
+  write.table(inflection_points, file=name, sep=",")
+  print ("Inflection Points Found")
+  name <- paste0(y, "_Post")
+  iCegraph(x = predpulsewave, n=name)
+  print ("New Graph Added")
+}
+
+#' Runs pre are post exercise iCe functions and prepares output overlay graph
+#'
+#' @param x pre exercise dataset created by the EndoPAT graph
+#' @param y post exercise dataset created by the EndoPAT graph
+#' @param n string used to name the outputs
+#'
+#' @return .csv files of inflection points (minima in first derivative)
+#' @return .text files with description of linear regression of first portion of pulse pressure graph
+#'
+#' @return PulseWave.png of Pulse pressure graph
+#' @return FirstDev.png of Pulse pressure graph first derivative
+#' @return InitialSlope.png of Initial linear portion of the pulse pressure graph
+#' @export iCe_demo
+#'
+#' @examples iCe_demo("data_PreT.csv", "data_PostT.csv", "Patient_name")
+iCe_demo <- function(x, y, n) {
+  m <<- "Pre"
+  iCe_pre(x, n)
+  m <<- "Post"
+  iCe_post(y, n)
+  Clientgraph(Pre, Post, n)
+
+  #Clean Up
+  rm(data2, envir = .GlobalEnv)
+  rm(FinalMaxima4, envir = .GlobalEnv)
+  rm(FinalMinima4, envir = .GlobalEnv)
+  rm(MaximaRows, envir = .GlobalEnv)
+  rm(MinimaRows, envir = .GlobalEnv)
+  rm(predpulsewave, envir = .GlobalEnv)
+  rm(dapulsewave, envir = .GlobalEnv)
+  rm(inflection_points, envir = .GlobalEnv)
+  rm(m, envir = .GlobalEnv)
+  rm(newvals2, envir = .GlobalEnv)
+  rm(Pre, envir = .GlobalEnv)
+  rm(Post, envir = .GlobalEnv)
+}
+
+
---
+++ R/Image_Extraction.R
@@ -0,0 +1,29 @@
+#' Image Extraction
+#' Takes in a png image of a line graph (which is ostensibly a screenshot of EndoPat) and identifies all the pixels which are associated with a line on the line graph
+#'
+#' @param x "screenshot.png"
+#' @param y "Name_ExerciseStatus"
+#' @param z "Treatment"
+#'
+#' @return Person_ExerciseStatus_TrtGroup.csv
+#'
+#' @import raster
+#' @export Image_Process
+#'
+#' @examples Image_Process("patient.png", "AndrewGow_Pre", "T")
+Image_Process <- function(x, y, z) {
+  name2 <- paste0(y, z, ".csv")
+
+  img <- raster(x)
+  #ext <- extent(0, 350, 0, 400) -- may be used in future to adjust issues with screenshotting size
+  #extent(img) <- ext
+  #print(extent(img))
+
+  df <- as.data.frame(img, xy = TRUE)
+  plot(img)
+
+  dt <- as.data.table(df)
+  subset_dt <- dt[which(dt[, 3] == 0), ]
+  data <- subset_dt[, c("x", "y")]
+  write.table(data, file = name2, row.names=F, col.names=F, sep=",")
+  }
